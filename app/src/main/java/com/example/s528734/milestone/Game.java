540648430629ab25565d817ea54550eecd5c8403
bhavishya <arellibhavishya@gmail.com>
2017-10-06 21:58:24 -0500

    undo, rotate

--- app/src/main/java/com/example/s528734/milestone/Game.java
+++ app/src/main/java/com/example/s528734/milestone/Game.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class Game extends AppCompatActivity {
 
@@ -16,5 +17,13 @@ public class Game extends AppCompatActivity {
     public void OnHomePage(View v){
         Intent homepage = new Intent(this, MainActivity.class);
         startActivity(homepage);
+    }
+    public void undo(View v)
+    {
+        Toast.makeText(getApplicationContext(), "last action is undone", Toast.LENGTH_LONG).show();
+    }
+    public void rotate(View v)
+    {
+        Toast.makeText(getApplicationContext(), "the selected block is rotated", Toast.LENGTH_LONG).show();
     }
 }
