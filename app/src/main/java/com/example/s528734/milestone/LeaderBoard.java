bfa383b25b261649ab7ba79c8d1d85fe5a381bfa
AvinashVasadi <s528770@mail.nwmissouri.edu>
2017-10-06 20:44:52 -0500

    updated Intents for activities connectivity.

--- app/src/main/java/com/example/s528734/milestone/LeaderBoard.java
+++ app/src/main/java/com/example/s528734/milestone/LeaderBoard.java
@@ -1,7 +1,9 @@
 package com.example.s528734.milestone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LeaderBoard extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class LeaderBoard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leader_board);
+    }
+    public void LeaderBoardOnBack(View v){
+        Intent bcak = new Intent(this, MainActivity.class);
+        startActivity(bcak);
     }
 }
--- app/src/main/java/com/example/s528734/milestone/score.java
+++ app/src/main/java/com/example/s528734/milestone/score.java
@@ -1,7 +1,9 @@
 package com.example.s528734.milestone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class score extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class score extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+    }
+
+    public void ScoreBoardOnBack(View v){
+        Intent back = new Intent(this, MainActivity.class);
+        startActivity(back);
     }
 }
