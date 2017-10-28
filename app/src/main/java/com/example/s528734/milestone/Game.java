e48cc707ac20b297affd8cf121770414f86dc545
AvinashVasadi <s528770@mail.nwmissouri.edu>
2017-10-28 17:32:52 -0500

    code Updated to passing picture to other activity and splitting the image

--- app/src/main/java/com/example/s528734/milestone/Game.java
+++ app/src/main/java/com/example/s528734/milestone/Game.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
@@ -17,6 +22,8 @@ import java.io.FileDescriptor;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Game extends AppCompatActivity {
 
@@ -24,6 +31,39 @@ public class Game extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        Bundle extras = getIntent().getExtras();
+        byte[] b = extras.getByteArray("bmp_img");
+//
+      Bitmap bmp = BitmapFactory.decodeByteArray(b, 0, b.length);
+
+        Bitmap bmOverlay = Bitmap.createBitmap(315, 315, Bitmap.Config.ARGB_8888);
+
+        Paint paint = new Paint();
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+
+        Canvas canvas = new Canvas(bmOverlay);
+        canvas.drawBitmap(bmp, 0, 0, null);
+        //canvas.drawRect(0, 0, 100, 100, paint);
+
+//        Bitmap[] imgs = new Bitmap[9];
+//        imgs[0] = Bitmap.createBitmap(bmOverlay, 0, 0, 80 , 80);
+//        imgs[1] = Bitmap.createBitmap(bmOverlay, 80, 0, 80, 80);
+//        imgs[2] = Bitmap.createBitmap(bmOverlay,160, 0, 80,80);
+//        imgs[3] = Bitmap.createBitmap(bmOverlay, 0, 80, 80, 80);
+//        imgs[4] = Bitmap.createBitmap(bmOverlay, 80, 80, 80,80);
+//        imgs[5] = Bitmap.createBitmap(bmOverlay, 160, 80,80,80);
+//        imgs[6] = Bitmap.createBitmap(bmOverlay, 0, 160, 80,80);
+//        imgs[7] = Bitmap.createBitmap(bmOverlay, 80, 160,80,80);
+//        imgs[8] = Bitmap.createBitmap(bmOverlay, 160,160,80,80);
+//
+
+        ImageView iv = (ImageView)findViewById(R.id.imageView);
+        Bitmap result = splitImage(bmOverlay, 50);
+        iv.setImageBitmap(result);
+
+        //Bitmap mBitmap = (Bitmap) getIntent().getParcelableExtra("bmp_img");
+
 
 //        Bundle extras = getIntent().getExtras();
 //        byte[] byteArray = extras.getByteArray("picture");
@@ -47,57 +87,69 @@ public class Game extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), "the selected block is rotated", Toast.LENGTH_LONG).show();
     }
 
-//    private static int RESULT_LOAD_IMAGE = 1;
-//    public void onBrowse(View v) {
-//
-//        Intent i = new Intent(
-//                Intent.ACTION_PICK,
-//                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-//
-//        startActivityForResult(i, RESULT_LOAD_IMAGE);
-//
-//    }
-//
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
-//            Uri selectedImage = data.getData();
-//            String[] filePathColumn = { MediaStore.Images.Media.DATA };
-//
-//            Cursor cursor = getContentResolver().query(selectedImage,
-//                    filePathColumn, null, null, null);
-//            cursor.moveToFirst();
-//
-//            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-//            String picturePath = cursor.getString(columnIndex);
-//            cursor.close();
-//
-//            //ImageView imageView = (ImageView) findViewById(R.id.imgView);
-//
-//            Bitmap bmp = null;
-//            try {
-//                bmp = getBitmapFromUri(selectedImage);
-//            } catch (IOException e) {
-//                // TODO Auto-generated catch block
-//                e.printStackTrace();
-//            }
-//           // imageView.setImageBitmap(bmp);
-//
-//        }
-//
-//
-//    }
-//
-//
-//
-//    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
-//        ParcelFileDescriptor parcelFileDescriptor =
-//                getContentResolver().openFileDescriptor(uri, "r");
-//        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
-//        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
-//        parcelFileDescriptor.close();
-//        return image;
-//    }
-}
+    ArrayList<Bitmap> chunkedImages;
+
+    private Bitmap splitImage(Bitmap image, int chunkNumbers) {
+        Bitmap bitmap = image;
+        //For the number of rows and columns of the grid to be displayed
+        int rows,cols;
+
+        //For height and width of the small image chunks
+        int chunkHeight,chunkWidth;
+
+        //To store all the small image chunks in bitmap format in this list
+        chunkedImages = new ArrayList<Bitmap>(chunkNumbers);
+
+        //Getting the scaled bitmap of the source image
+
+    /*ByteArrayOutputStream stream = new ByteArrayOutputStream();
+    bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);*/
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, bitmap.getWidth(), bitmap.getHeight(), true);
+
+        rows = cols = (int) Math.sqrt(chunkNumbers);
+        chunkHeight = bitmap.getHeight()/rows;
+        chunkWidth = bitmap.getWidth()/cols;
+    /*chunkHeight = 300/rows;
+    chunkWidth = 300/cols;*/
+
+        //xCoord and yCoord are the pixel positions of the image chunks
+        int yCoord = 0;
+        for(int x=0; x<rows; x++){
+            int xCoord = 0;
+            for(int y=0; y<cols; y++){
+                chunkedImages.add(Bitmap.createBitmap(scaledBitmap, xCoord, yCoord, chunkWidth, chunkHeight));
+                xCoord += chunkWidth;
+            }
+            yCoord += chunkHeight;
+        }
+
+        // Function of merge the chunks images(after image divided in pieces then i can call this function to combine and merge the image as one)
+        Bitmap x = mergeImage(chunkedImages);
+
+        return x;
+    }
+
+    Bitmap mergeImage(ArrayList<Bitmap> imageChunks) {
+
+        Collections.shuffle(imageChunks);
+
+        //Get the width and height of the smaller chunks
+        int chunkWidth = imageChunks.get(0).getWidth();
+        int chunkHeight = imageChunks.get(0).getHeight();
+
+        //create a bitmap of a size which can hold the complete image after merging
+        Bitmap bitmap = Bitmap.createBitmap(chunkWidth * 5, chunkHeight * 5, Bitmap.Config.ARGB_4444);
+
+        //create a canvas for drawing all those small images
+        Canvas canvas = new Canvas(bitmap);
+        int count = 0;
+        for (int rows = 0; rows < 5; rows++) {
+            for (int cols = 0; cols < 5; cols++) {
+                canvas.drawBitmap(imageChunks.get(count), chunkWidth * cols, chunkHeight * rows, null);
+                count++;
+            }
+        }
+        return bitmap;
+    }
+
+    }
