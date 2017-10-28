82bf47018ce7d3c6414b7800f805102ba2947a81
AvinashVasadi <s528770@mail.nwmissouri.edu>
2017-10-27 23:22:22 -0500

    new update

--- app/src/main/java/com/example/s528734/milestone/Game.java
+++ app/src/main/java/com/example/s528734/milestone/Game.java
@@ -1,10 +1,22 @@
 package com.example.s528734.milestone;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Game extends AppCompatActivity {
 
@@ -12,6 +24,14 @@ public class Game extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+//        Bundle extras = getIntent().getExtras();
+//        byte[] byteArray = extras.getByteArray("picture");
+//
+//        Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+//        ImageView image = (ImageView) findViewById(R.id.imageView);
+//
+//        image.setImageBitmap(bmp);
     }
 
     public void OnHomePage(View v){
@@ -26,4 +46,58 @@ public class Game extends AppCompatActivity {
     {
         Toast.makeText(getApplicationContext(), "the selected block is rotated", Toast.LENGTH_LONG).show();
     }
+
+//    private static int RESULT_LOAD_IMAGE = 1;
+//    public void onBrowse(View v) {
+//
+//        Intent i = new Intent(
+//                Intent.ACTION_PICK,
+//                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//
+//        startActivityForResult(i, RESULT_LOAD_IMAGE);
+//
+//    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+//            Uri selectedImage = data.getData();
+//            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+//
+//            Cursor cursor = getContentResolver().query(selectedImage,
+//                    filePathColumn, null, null, null);
+//            cursor.moveToFirst();
+//
+//            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+//            String picturePath = cursor.getString(columnIndex);
+//            cursor.close();
+//
+//            //ImageView imageView = (ImageView) findViewById(R.id.imgView);
+//
+//            Bitmap bmp = null;
+//            try {
+//                bmp = getBitmapFromUri(selectedImage);
+//            } catch (IOException e) {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//            }
+//           // imageView.setImageBitmap(bmp);
+//
+//        }
+//
+//
+//    }
+//
+//
+//
+//    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+//        ParcelFileDescriptor parcelFileDescriptor =
+//                getContentResolver().openFileDescriptor(uri, "r");
+//        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+//        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+//        parcelFileDescriptor.close();
+//        return image;
+//    }
 }
--- app/src/main/java/com/example/s528734/milestone/PregameSettings.java
+++ app/src/main/java/com/example/s528734/milestone/PregameSettings.java
@@ -1,9 +1,24 @@
 package com.example.s528734.milestone;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class PregameSettings extends AppCompatActivity {
 
@@ -21,6 +36,69 @@ public class PregameSettings extends AppCompatActivity {
     public void OnLetsGo(View v){
         Intent letsgo = new Intent(this, Game.class);
         startActivity(letsgo);
+
+        Intent intent = new Intent(this, Game.class);
+        intent.putExtra("picture", byteArray);
+        startActivity(intent);
     }
 
+    private static int RESULT_LOAD_IMAGE = 1;
+
+    public byte[] byteArray;
+
+    public void onBrowse(View v) {
+
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, RESULT_LOAD_IMAGE);
+//        Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
+//        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+//        bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+//        byteArray = stream.toByteArray();
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            //ImageView imageView = (ImageView) findViewById(R.id.imgView);
+
+            Bitmap bmp = null;
+            try {
+                bmp = getBitmapFromUri(selectedImage);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            //imageView.setImageBitmap(bmp);
+
+        }
+
+
+    }
+
+
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
+    }
 }
