a998754ffa10bb5c4e53c9d77964f87a9a88f09f
uhas <saahuchintha@gmail.com>
2017-11-17 22:38:57 -0600

    Added audio to game

--- app/src/main/java/com/example/s528734/milestone/MainActivity.java
+++ app/src/main/java/com/example/s528734/milestone/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.s528734.milestone;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mediaPlayer = MediaPlayer.create(this, R.raw.musicbackground);
+        mediaPlayer.start();
     }
 
     public void onStart(View v){
