9c8e025c17b3c328a2c384d824b79ae4d7238492
BhavishyaArelli <s528734@mail.nwmissouri.edu>
2017-11-18 20:57:28 -0600

    Updated the code for hard level, it will also display the warning image when incorrect size image is selected.

--- app/src/main/java/com/example/s528734/milestone/GameMedium.java
+++ app/src/main/java/com/example/s528734/milestone/GameMedium.java
@@ -1,39 +1,27 @@
 package com.example.s528734.milestone;
 
 import android.content.Intent;
-import android.database.Cursor;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
 import android.graphics.drawable.BitmapDrawable;
-import android.net.Uri;
-import android.os.ParcelFileDescriptor;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.io.FileDescriptor;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
 public class GameMedium extends AppCompatActivity {
 
     Bitmap[] imgs = new Bitmap[16];
+    Bitmap[] errimg = new Bitmap[9];
     Bitmap[] refimgs = new Bitmap[16];
     int position1 = 20;
     int position2 = 20;
@@ -52,66 +40,94 @@ public class GameMedium extends AppCompatActivity {
 
         Bitmap bmp = BitmapFactory.decodeByteArray(b, 0, b.length);
 
-        int xcordinateindex = (bmp.getWidth()-300)/2;
-        int ycordinateindex = (bmp.getHeight()-300)/2;
+        int length = bmp.getWidth();
+        int width = bmp.getHeight();
 
-        Bitmap bmOverlay = Bitmap.createBitmap(bmp, xcordinateindex,ycordinateindex, 300, 300);
+        if(length>300 && width>300){
 
-        imgs[0] = Bitmap.createBitmap(bmOverlay, 0, 0, 75,  75);
-        imgs[1] = Bitmap.createBitmap(bmOverlay, 75, 0, 75, 75);
-        imgs[2] = Bitmap.createBitmap(bmOverlay, 150, 0, 75, 75);
-        imgs[3] = Bitmap.createBitmap(bmOverlay, 225, 0, 75, 75);
-        imgs[4] = Bitmap.createBitmap(bmOverlay, 0, 75, 75, 75);
-        imgs[5] = Bitmap.createBitmap(bmOverlay, 75, 75, 75, 75);
-        imgs[6] = Bitmap.createBitmap(bmOverlay, 150, 75, 75, 75);
-        imgs[7] = Bitmap.createBitmap(bmOverlay, 225, 75, 75, 75);
-        imgs[8] = Bitmap.createBitmap(bmOverlay, 0, 150, 75, 75);
-        imgs[9] = Bitmap.createBitmap(bmOverlay, 75, 150, 75, 75);
-        imgs[10] = Bitmap.createBitmap(bmOverlay, 150, 150, 75, 75);
-        imgs[11] = Bitmap.createBitmap(bmOverlay, 225, 150, 75, 75);
-        imgs[12] = Bitmap.createBitmap(bmOverlay, 0, 225, 75, 75);
-        imgs[13] = Bitmap.createBitmap(bmOverlay, 75, 225, 75, 75);
-        imgs[14] = Bitmap.createBitmap(bmOverlay, 150, 225, 75, 75);
-        imgs[15] = Bitmap.createBitmap(bmOverlay, 225, 225, 75, 75);
+            int xcordinateindex = (bmp.getWidth()-300)/2;
+            int ycordinateindex = (bmp.getHeight()-300)/2;
 
-        refimgs = imgs.clone();
+            Bitmap bmOverlay = Bitmap.createBitmap(bmp, xcordinateindex,ycordinateindex, 300, 300);
 
-        Collections.shuffle((Arrays.asList(imgs)));
+            imgs[0] = Bitmap.createBitmap(bmOverlay, 0, 0, 75,  75);
+            imgs[1] = Bitmap.createBitmap(bmOverlay, 75, 0, 75, 75);
+            imgs[2] = Bitmap.createBitmap(bmOverlay, 150, 0, 75, 75);
+            imgs[3] = Bitmap.createBitmap(bmOverlay, 225, 0, 75, 75);
+            imgs[4] = Bitmap.createBitmap(bmOverlay, 0, 75, 75, 75);
+            imgs[5] = Bitmap.createBitmap(bmOverlay, 75, 75, 75, 75);
+            imgs[6] = Bitmap.createBitmap(bmOverlay, 150, 75, 75, 75);
+            imgs[7] = Bitmap.createBitmap(bmOverlay, 225, 75, 75, 75);
+            imgs[8] = Bitmap.createBitmap(bmOverlay, 0, 150, 75, 75);
+            imgs[9] = Bitmap.createBitmap(bmOverlay, 75, 150, 75, 75);
+            imgs[10] = Bitmap.createBitmap(bmOverlay, 150, 150, 75, 75);
+            imgs[11] = Bitmap.createBitmap(bmOverlay, 225, 150, 75, 75);
+            imgs[12] = Bitmap.createBitmap(bmOverlay, 0, 225, 75, 75);
+            imgs[13] = Bitmap.createBitmap(bmOverlay, 75, 225, 75, 75);
+            imgs[14] = Bitmap.createBitmap(bmOverlay, 150, 225, 75, 75);
+            imgs[15] = Bitmap.createBitmap(bmOverlay, 225, 225, 75, 75);
 
-        startTime = System.currentTimeMillis();
+            refimgs = imgs.clone();
 
-        final GridView gridview = (GridView) findViewById(R.id.gridviewMedium);
-        gridview.setAdapter(new ImageAdapter(this, imgs));
+            Collections.shuffle((Arrays.asList(imgs)));
 
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(final AdapterView<?> parent, View view, final int position, long id) {
-                ImageView tile1 = (ImageView) gridview.getChildAt(position);
-                ImageView capture1 = (ImageView) tile1;
-                capture1.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Toast.makeText(getApplicationContext(), "Accessed Tile : "+position, Toast.LENGTH_LONG).show();
-                        if(position1ref == 0){
-                            Log.d("entered 1st click", "Click1");
-                            position1 = position;
-                            position1ref = 1;
+            startTime = System.currentTimeMillis();
+
+            final GridView gridview = (GridView) findViewById(R.id.gridviewMedium);
+            gridview.setAdapter(new ImageAdapter(this, imgs));
+
+            gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(final AdapterView<?> parent, View view, final int position, long id) {
+                    ImageView tile1 = (ImageView) gridview.getChildAt(position);
+                    ImageView capture1 = (ImageView) tile1;
+                    capture1.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Toast.makeText(getApplicationContext(), "Accessed Tile : "+position, Toast.LENGTH_LONG).show();
+                            if(position1ref == 0){
+                                Log.d("entered 1st click", "Click1");
+                                position1 = position;
+                                position1ref = 1;
+                            }
+                            if(position2ref == 0){
+                                Log.d("entered 2nd click", "Click2");
+                                position2 = position;
+                            }
+                            if(position1ref == 1){
+                                position2ref = 0;
+                            }
+                            if(position1 != 20 && position2 != 20){
+                                Log.d("entered 2nd click", "swapped");
+                                swap(position1, position2);
+                            }
                         }
-                        if(position2ref == 0){
-                            Log.d("entered 2nd click", "Click2");
-                            position2 = position;
-                        }
-                        if(position1ref == 1){
-                            position2ref = 0;
-                        }
-                        if(position1 != 20 && position2 != 20){
-                            Log.d("entered 2nd click", "swapped");
-                            swap(position1, position2);
-                        }
-                    }
-                });
-            }
-        });
+                    });
+                }
+            });
+        } else {
+            Resources resources = getResources();
+            Drawable d = resources.getDrawable(R.drawable.error);
+            Bitmap errorimgy = ((BitmapDrawable) d).getBitmap();
+
+            int xcordinateindexy = (errorimgy.getWidth()-300)/2;
+            int ycordinateindexy = (errorimgy.getHeight()-300)/2;
+
+            Bitmap errorimg = Bitmap.createBitmap(errorimgy, xcordinateindexy,ycordinateindexy, 300, 300);
+            errimg[0] = Bitmap.createBitmap(errorimg, 0, 0, 100,  100);
+            errimg[1] = Bitmap.createBitmap(errorimg, 100, 0, 100, 100);
+            errimg[2] = Bitmap.createBitmap(errorimg, 200, 0, 100, 100);
+            errimg[3] = Bitmap.createBitmap(errorimg, 0, 100, 100, 100);
+            errimg[4] = Bitmap.createBitmap(errorimg, 100, 100, 100, 100);
+            errimg[5] = Bitmap.createBitmap(errorimg, 200, 100, 100, 100);
+            errimg[6] = Bitmap.createBitmap(errorimg, 0, 200, 100, 100);
+            errimg[7] = Bitmap.createBitmap(errorimg, 100, 200, 100, 100);
+            errimg[8] = Bitmap.createBitmap(errorimg, 200, 200, 100, 100);
+            final GridView gridview = (GridView) findViewById(R.id.gridview);
+            gridview.setAdapter(new ImageAdapter(this, errimg));
+
+        }
+
     }
 
     public void swap(int index1, int index2){
